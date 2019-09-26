cb627647f6d963568ba8197cbd666dbe4efab33d
fatninja <appotdar05@gmail.com>
2019-09-27 00:54:35 +0530

    code to create bin img

--- guide.py
+++ guide.py
@@ -7,15 +7,12 @@ import os
 
 #Conver Img to Biary format
 img = cv2.imread('/home/amarp/Documents/pyproj/CV/forFoilio/cv_guide/imgs/cell_bin.png')
-ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
 temp = thresh[:,:,0]
-temp[temp == 255] = 1
-# cv2.imwrite('cellsnew_bin.jpg',temp)
-# plt.imshow(temp)
+cv2.imwrite('cell_bin.png',temp)
+plt.imshow(temp)
 
-cv2.imshow('Img',temp)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 
 
