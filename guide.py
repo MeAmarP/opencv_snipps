cea17338fe9f42b587d4f88629a6cfb4d885c304
meamarp <appotdar05@gmail.com>
2019-10-22 19:45:45 +0530

    show img

--- guide.py
+++ guide.py
@@ -6,12 +6,13 @@ import matplotlib.pyplot as plt
 import os
 
 #Conver Img to Biary format
-img = cv2.imread('/home/amarp/Documents/pyproj/CV/forFoilio/cv_guide/imgs/cell_bin.png')
+img = cv2.imread('imgs\Baraja_de_UNO.jpg')
 # ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-temp = thresh[:,:,0]
-cv2.imwrite('cell_bin.png',temp)
-plt.imshow(temp)
+# ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+# temp = thresh[:,:,0]
+# cv2.imwrite('cell_bin.png',temp)
+cv2.imshow('img',img)
+cv2.waitKey(0)
 
 
 
